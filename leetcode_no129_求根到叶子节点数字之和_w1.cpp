5111d1421a37c4724254ae512b2f9e9bab4e210e
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-10-29 10:39:18 +0800

    Add files via upload

---
+++ leetcode_no129_求根到叶子节点数字之和_w1.cpp
@@ -0,0 +1,80 @@
+﻿/* ------------------------------------------------------------------|
+给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+
+例如，从根到叶子节点路径 1->2->3 代表数字 123。
+
+计算从根到叶子节点生成的所有数字之和。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例 1:
+
+输入: [1,2,3]
+    1
+   / \
+  2   3
+输出: 25
+解释:
+从根到叶子节点路径 1->2 代表数字 12.
+从根到叶子节点路径 1->3 代表数字 13.
+因此，数字总和 = 12 + 13 = 25.
+示例 2:
+
+输入: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+输出: 1026
+解释:
+从根到叶子节点路径 4->9->5 代表数字 495.
+从根到叶子节点路径 4->9->1 代表数字 491.
+从根到叶子节点路径 4->0 代表数字 40.
+因此，数字总和 = 495 + 491 + 40 = 1026.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sum-root-to-leaf-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	递归法
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了81.75%的用户
+*	内存消耗：12.3 MB, 在所有 C++ 提交中击败了30.71%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+int dfs(TreeNode* root, int preSum) {
+    if (root == nullptr) {
+        return 0;
+    }
+    int sum = preSum * 10 + root->val;
+    //走到叶节点的尾部了
+    if (root->left == nullptr && root->right == nullptr) {
+        return sum;
+    }
+    //下面还有叶节点，还可以继续搜索
+    else {
+        return dfs(root->left, sum) + dfs(root->right, sum);
+    }
+}
+int sumNumbers(TreeNode* root) {
+    return dfs(root, 0);
+}
